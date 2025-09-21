e9ec637ca21fdc2379183c4ec9cf296ceb6a3824
Muhammad Jazib <mjxdex@gmail.com>
2025-09-21 21:06:17 +0500

    Refactor Description and Invoice components for dynamic item management and update styling. Added sticky footer to CTA group and adjusted font styles in globals.sass.

--- components/Description.jsx
+++ components/Description.jsx
@@ -1,45 +1,31 @@
+"use client"
 import { Plus, X } from 'lucide-react'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Description = () => {
+    const [items, setItems] = useState([]);
     return (
         <>
             <div className="row">
                 <input type="text" placeholder='Description' />
                 <input type="number" placeholder='Qty' />
                 <input type="number" placeholder='Price' />
-                <div className="btn"><Plus /></div>
+                <div className="btn" onClick={() => { setItems(prev => [...prev, ""]) }}><Plus /></div>
             </div>
-            <div className="row">
-                <input type="text" placeholder='Description' />
-                <input type="number" placeholder='Qty' />
-                <input type="number" placeholder='Price' />
-                <div className="btn"><X /></div>
-            </div>
-            <div className="row">
-                <input type="text" placeholder='Description' />
-                <input type="number" placeholder='Qty' />
-                <input type="number" placeholder='Price' />
-                <div className="btn"><X /></div>
-            </div>
-            <div className="row">
-                <input type="text" placeholder='Description' />
-                <input type="number" placeholder='Qty' />
-                <input type="number" placeholder='Price' />
-                <div className="btn"><X /></div>
-            </div>
-            <div className="row">
-                <input type="text" placeholder='Description' />
-                <input type="number" placeholder='Qty' />
-                <input type="number" placeholder='Price' />
-                <div className="btn"><X /></div>
-            </div>
-            <div className="row">
-                <input type="text" placeholder='Description' />
-                <input type="number" placeholder='Qty' />
-                <input type="number" placeholder='Price' />
-                <div className="btn"><X /></div>
-            </div>
+            {
+                items.map((_, e) => (
+                    <div key={e} className="row">
+                        <input type="text" placeholder='Description' />
+                        <input type="number" placeholder='Qty' />
+                        <input type="number" placeholder='Price' />
+                        <div className="btn" onClick={() => {
+                            const copy = [...items];
+                            copy.splice(e, 1);
+                            setItems(copy);
+                        }} ><X /></div>
+                    </div>
+                ))
+            }
         </>
     )
 }
